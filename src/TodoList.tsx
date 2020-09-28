388b89941f22939edee10108ab63db6a3ce283fd
fokolo <talpaz.roi@gmail.com>
2020-09-28 19:52:01 +0300

    Gmar Hatima Tova

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -4,20 +4,21 @@ import { TodoListItem } from './TodoListItem';
 
 
 interface Props {
-    todos: Todo[];
+  todos: Todo[];
 }
 
-export const TodoList: React.FC<Props> = ({ todos }) => {
-    return (<>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableBody>
-              {todos.map(todoItem => (
-                  <TodoListItem todo={todoItem}/>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
 
-    </>);
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (<>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          {todos.map(todoItem => (
+            <TodoListItem todo={todoItem} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+  </>);
 }
--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -5,18 +5,26 @@ interface Props {
     todo: Todo;
 }
 
+const costStyle = {
+    maxWidth: '80px',
+    padding: '0 10px 0 0'
+}
+
 export const TodoListItem: React.FC<Props> = ({ todo }) => {
     return (<>
-        <TableRow>
+        <TableRow style={{
+            whiteSpace: 'normal',
+            wordWrap: 'break-word'
+        }}>
             <TableCell padding="checkbox">
                 <Checkbox checked={todo.complete} />
             </TableCell>
-            <TableCell>
-                <Typography>
+            <TableCell >
+                <Typography noWrap>
                     {todo.text}
                 </Typography>
             </TableCell>
-            <TableCell align={'right'} >
+            <TableCell align={'right'} style={costStyle}>
                 <Typography >${todo.cash}</Typography>
             </TableCell>
         </TableRow>
