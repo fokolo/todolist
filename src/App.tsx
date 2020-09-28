09b45879d9073b90232c474bf8152d808632106e
fokolo <talpaz.roi@gmail.com>
2020-09-28 12:24:58 +0300

    Added babel plugin for tree-shaking

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,44 @@
 import React, { useState } from 'react';
-import { TodoList } from './TodoList';
-import { AddTodoForm } from './AddTodoForm';
-
-const initialTodos: Todo[] = [
-  {
-    text: 'Task A',
-    complete: true,
-  },
-  {
-    text: 'Task B',
-    complete: false,
-  }
-]
+import { Backdrop, Checkbox, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from '@material-ui/core';
+import { CheckCircle } from '@material-ui/icons';
+import { green } from '@material-ui/core/colors';
 
 function App() {
-  const [todos, setTodos] = useState(initialTodos);
+  const [isSelected, setIsSelected] = useState(false);
+  const [backdropOpen, setBackdropOpen] = useState(true);
 
-  const toggleTodo = (selectedTodo: Todo) => {
-    const newTodos = todos.map(todo => {
-      if (todo === selectedTodo) {
-        return {
-          ...todo,
-          complete: !todo.complete,
-        };
-      }
-      return todo;
-    });
-
-    setTodos(newTodos);
-  }
-
-  const addTodo: AddTodo = (todoText) => {
-    const newTodo: Todo = {
-      text: todoText,
-      complete: false
-    }
-    setTodos([
-      ...todos,
-      newTodo
-    ]);
-  }
 
   return (
     <>
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <AddTodoForm addTodo={addTodo} />
+      <Backdrop open={backdropOpen} onClick={() => { setBackdropOpen(false) }}>
+        <CheckCircle style={{ color: green[500], fontSize: 40 }} />
+      </Backdrop>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableBody>
+            <TableRow selected={isSelected} hover onClick={() => {
+              setIsSelected(!isSelected);
+            }}>
+              <TableCell padding="checkbox">
+                <Checkbox checked={isSelected} onChange={(e) => {
+                  setIsSelected(e.target.checked);
+                }} />
+              </TableCell>
+              <TableCell>
+                Task A
+            </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell padding="checkbox">
+                <Checkbox />
+              </TableCell>
+              <TableCell>
+                Task B
+            </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>);
 }
 
